6fe31a556ea51ce888cd09e88e132db16203d4ab
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2023-05-20 00:31:49 +0300

    publishing configurations to use java 17 instead of 11

--- BoilrBite/build.gradle.kts
+++ BoilrBite/build.gradle.kts
@@ -62,7 +62,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.abdomi7"
             artifactId = "BoilrBite"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
